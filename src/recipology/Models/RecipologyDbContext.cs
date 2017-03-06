fc14add381696a3fd9bbd1d11029a7fa89ca21b3
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-03-06 14:49:30 -0800

    add empty files for required models

---
+++ src/recipology/Models/RecipologyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipology.Models
+{
+    public class RecipologyDbContext
+    {
+    }
+}
