d32da650128437f1836e82333afe17d4ee13b5d8
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-03-07 08:46:04 -0800

    Implement initial applicationdbcontext

--- src/recipology/Models/RecipologyDbContext.cs
+++ src/recipology/Models/RecipologyDbContext.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Recipology.Models
 {
-    public class RecipologyDbContext
+    public class RecipologyDbContext : DbContext
     {
+        public RecipologyDbContext()
+        {
+            
+        }
+        public DbSet<Recipe> Recipes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Recipology;integrated security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
+
 }
