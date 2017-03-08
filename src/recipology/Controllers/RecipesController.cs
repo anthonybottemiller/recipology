fba3c83c661d8bc2bcaf4d791ae4c7de256910d2
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-03-07 17:52:28 -0800

    Rename Recipes controller

---
+++ src/recipology/Controllers/RecipesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Recipology.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace recipology.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private RecipologyDbContext db = new RecipologyDbContext();
+
+        public IActionResult Top()
+        {
+            return View(db.Recipes.ToList());
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Create(Recipe recipe)
+        {
+            db.Recipes.Add(recipe);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
