722acb63fdbb90cab159c74df6a8e13dcaa787f5
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-03-07 17:28:59 -0800

    implement view models for identity

--- src/recipology/Models/RecipologyDbContext.cs
+++ src/recipology/Models/RecipologyDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using recipology.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
