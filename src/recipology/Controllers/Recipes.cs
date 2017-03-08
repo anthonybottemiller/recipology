4fb61cf2166af41cbcb4646f3c58bff94a4797be
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-03-07 16:38:19 -0800

    add recipes controller

---
+++ src/recipology/Controllers/Recipes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Recipology.Models;
+
+namespace recipology.Controllers
+{
+    public class Recipes : Controller
+    {
+        private RecipologyDbContext db = new RecipologyDbContext();
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Recipe recipe)
+        {
+            db.Recipes.Add(recipe);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
